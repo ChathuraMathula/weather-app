7ae9528057ed197714b7be95473290fbaf35fd74
chathuramathula <chathuraekanayake97@gmail.com>
2023-11-16 11:10:32 +0530

    refactored WeatherApp.jsx component

--- front-end/src/jsx/components/WeatherApp.jsx
+++ front-end/src/jsx/components/WeatherApp.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useState } from 'react'
 import '../../css/App.css';
 
 import { fetchCachedWeatherData, cacheWeatherData, isCachedDataExpired } from '../../js/helpers/localStorageHelpers';
